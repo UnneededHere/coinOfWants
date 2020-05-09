3167754977251127c71d158003b4a1ad51cf1b52
Kyle Scorgie <krjs@outlook.com>
2020-05-09 16:07:55 +0100

    Everything set up bar actual algorithm

--- canWeOrganise/client/main.js
+++ canWeOrganise/client/main.js
@@ -2,7 +2,7 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
-import { Events } from '../import/api/wacky';
+import { Events, Persons } from '../import/api/wacky';
 import { Mongo } from 'meteor/mongo';
 
 Router.route('/home', function() {
@@ -15,6 +15,9 @@ Router.route('/event/:_id', {
   template: 'eventPage',
   data: function() {
     var currentEvent = new Mongo.ObjectID(this.params._id);
-    return Events.findOne({ _id: currentEvent });
+    return {
+      eventDetails: Events.findOne({ _id: currentEvent }),
+      peopleNeeds: Persons.find({ relEvent: this.params._id })
+    }
   }
 })
--- canWeOrganise/import/api/wacky.js
+++ canWeOrganise/import/api/wacky.js
@@ -1,3 +1,4 @@
 import { Meteor } from "meteor/meteor";
 
 export const Events = new Meteor.Collection('events');
+export const Persons = new Meteor.Collection('persons');
